dd3ef99319fb1716af6beae3a97a618162e302da
Erica-Brock <Eady.Brock@gmail.com>
2017-10-09 17:07:52 -0500

    created sumOf function

--- scripts.js
+++ scripts.js
@@ -1,23 +1,28 @@
 // 1.Crate three buttons(done in HTML)
-
+var sum;
 var values=[{value:1,
-    image:url("dicey-images/dice-1.png")},
+    image:"url('dicey-images/dice-1.png')"},
     {value: 2,
-    image:url("dicey-images/dice-2.png")},
+    image:"url('dicey-images/dice-2.png')"},
     {value:3,
-    image:url("dicey-images/dice-3.png")},
+    image:"url('dicey-images/dice-3.png')"},
     {value:4,
-    image:url("dicey-images/dice-4.png")},
+    image:"url('dicey-images/dice-4.png')"},
     {value:5,
-    image:url("dicey-images/dice-5.jpeg")},
+    image:"url('dicey-images/dice-5.jpeg')"},
     {value:6,
-    image:url("dicey-images/dice-6.png")}];
+    image:"url('dicey-images/dice-6.png')"}];
 var newDieValues=[];
 document.addEventListener("DOMContentLoaded", function(){
     var newDie=document.getElementById("new-die");
+    var totalDice=document.getElementById("sum");
+    var roll=document.getElementById("roll");
     newDie.addEventListener("click", function(){
        new Die();
-    }
+    },
+    totalDice.addEventListener("click", function(){
+        sumOf()
+   })
 )
 })
 //2. create a constructor function()
@@ -25,14 +30,25 @@ function Die (){
     this.div= document.createElement('div');
     this.div.className="dice";
     document.body.appendChild(this.div);
+    this.value= random();
+    //attach the random image to the background
+    this.div.style.backgroundImage=this.value.image;
+    newDieValues.push(this.value.value);
+    console.log(newDieValues);
 }
-/*Die.prototype.roll=function(){
-    this.value = values[Math.floor(Math.random()*values.length)];
-console.log(this.value);
+//get the sum of the newDieValues array
+function sumOf(){
+    sum = newDieValues.reduce(add, 0);
+function add(a, b) {
+    return a + b;
 }
-var o= new Die(this.value);
-o.roll();*/
+console.log(sum);
+alert("Sum of dice equals: "+sum);
 
-    //it should have a value property
-    //it shoulld also have a function called roll
-//3.
+ }
+
+//get a random die from the array
+function random(){ 
+   return values[Math.floor(Math.random()*values.length)];
+}
+
