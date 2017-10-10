8125189d92552cb5f53f389a158b1fb376f2366f
Erica-Brock <Eady.Brock@gmail.com>
2017-10-10 10:10:47 -0500

    addded remove function

--- scripts.js
+++ scripts.js
@@ -1,73 +1,73 @@
 // 1.Create three buttons(done in HTML)
 var sum;
-var values=[{value:1,
-    image:"url('dicey-images/dice-1.png')"},
-    {value: 2,
-    image:"url('dicey-images/dice-2.png')"},
-    {value:3,
-    image:"url('dicey-images/dice-3.png')"},
-    {value:4,
-    image:"url('dicey-images/dice-4.png')"},
-    {value:5,
-    image:"url('dicey-images/dice-5.jpeg')"},
-    {value:6,
-    image:"url('dicey-images/dice-6.png')"}];
-var newDieValues=[];
-document.addEventListener("DOMContentLoaded", function(){
-    var newDie=document.getElementById("new-die");
-    var totalDice=document.getElementById("sum");
-    var roll=document.getElementById("roll");
-    newDie.addEventListener("click", function(){
-       newDieValues.push(new Die());
-    },
-    totalDice.addEventListener("click", function(){
+var values = [{ value: 1, image: "url('dicey-images/dice-1.png')" },
+{ value: 2, image: "url('dicey-images/dice-2.png')" },
+{ value: 3, image: "url('dicey-images/dice-3.png')" },
+{ value: 4, image: "url('dicey-images/dice-4.png')" },
+{ value: 5, image: "url('dicey-images/dice-5.jpeg')" },
+{ value: 6, image: "url('dicey-images/dice-6.png')" }];
+var newDieValues = [];
+document.addEventListener("DOMContentLoaded", function () {
+    var newDie = document.getElementById("new-die");
+    var totalDice = document.getElementById("sum");
+    var roll = document.getElementById("roll");
+    newDie.addEventListener("click", function () {
+        newDieValues.push(new Die());
+    })
+    totalDice.addEventListener("click", function () {
         sumOf()
-   }),
-   roll.addEventListener("click", function(){
-       rollAll();
-   })
-)
+    })
+    roll.addEventListener("click", function () {
+        rollAll();
+    })
 })
 //2. create a constructor function()
-function Die (){
-    this.div= document.createElement('div');
-    this.div.className="dice";
+function Die() {
+    this.div = document.createElement('div');
+    this.div.className = "dice";
     document.body.appendChild(this.div);
-    this.info= random();
+    this.info = random();
     //attach the random image to the background
-    this.div.style.backgroundImage=this.info.image;
+    this.div.style.backgroundImage = this.info.image;
+    //remove the die from the page when double clicked
+    this.div.addEventListener("dblclick", this.removeOne.bind(this))
+    //make the value, and image change when clicked
     this.div.addEventListener("click", this.rollOne.bind(this))
 
 }
-Die.prototype.rollOne=function(){
-       var changeOne= newDieValues.indexOf(this);
-       newValue=random();
-       change=newDieValues[changeOne];
-       change.info.value=newValue.value;
-       change.info.image=newValue.image;
-       console.log(newDieValues);
-       this.div.style.backgroundImage=newValue.image;
+//rolling the die
+Die.prototype.rollOne = function () {
+    //how to find the index of the object in the array
+    var changeOne = newDieValues.indexOf(this);
+    newValue = random();
+    change = newDieValues[changeOne];
+    //changing the value and image
+    change.info.value = newValue.value;
+    //change.info.image = newValue.image;
+    this.div.style.backgroundImage = newValue.image;
 }
-function rollAll(){
-    for(var i=0; i<newDieValues.length; i++){
-       this.rollOne.bind(this.div)
+Die.prototype.removeOne = function () {
+    var remove = newDieValues.indexOf(this);
+    newDieValues.splice(remove, 1);
+    document.body.removeChild(this.div);
+}
+function rollAll() {
+    for (var i = 0; i < newDieValues.length; i++) {
+        let changeOne = newDieValues[i];
+        let change = changeOne;
+        newDieValues[i].rollOne()
     }
 }
-
-
 //get the sum of the newDieValues array
-function sumOf(){
-    sum = newDieValues.reduce (function (a, b) {
-    return a + b.info.value;
-
-},0);
-console.log(newDieValues);
-alert("Sum of dice equals: "+sum);
-
- }
-
+function sumOf() {
+    sum = newDieValues.reduce(function (a, b) {
+        return a + b.info.value;
+    }, 0);
+    alert("Sum of dice equals: " + sum);
+    console.log(newDieValues);
+}
 //get a random die from the array
-function random(){ 
-   return values[Math.floor(Math.random()*values.length)];
+function random() {
+    return values[Math.floor(Math.random() * values.length)];
 }
 
